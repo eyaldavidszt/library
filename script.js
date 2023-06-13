d34124df49e78138a9863758adab60b838fe61bf
Hellohellohello12313 <66707792+hellohellohello12321@users.noreply.github.com>
2023-06-14 02:37:05 +0300

    Complete function which parses book array and creates dom elements for each

--- script.js
+++ script.js
@@ -23,7 +23,11 @@ const bookOne = new Book('The Hobbit', 'J.R.R. Tolkien', '295', false);
 console.log(bookOne.info());
 
 function addBookToLibrary() {
+    // get user input and call constructor function with it
+    myLibrary.push(bookOne);
   }
+
+addBookToLibrary();
   
 //**********************
 
@@ -34,3 +38,52 @@ function addBookToLibrary() {
 
 //on remove book, find index="" attribute with js, then remove the object in that index from the array, 
 //  and also remove the element associated with it from the page. 
+function loadArray() {
+    for (const book of myLibrary) {
+        let title = book.title;
+        let author = book.author;
+        let pages = book.pages;
+        let read = book.read ? 'Read' : 'Not read yet'; 
+        
+        let randRed = Math.floor((Math.random() * 150) + 50);
+        let randGreen = Math.floor((Math.random() * 150) + 50);
+        let randBlue = Math.floor((Math.random() * 200) + 1);
+
+        let newBook = document.createElement('div');
+        newBook.classList.add('book');
+
+        let titleHeader = document.createElement('h1');
+        titleHeader.textContent = title;
+
+        let authorHeader = document.createElement('h2');
+        authorHeader.textContent = author;
+
+        let pagesHeader = document.createElement('h3');
+        pagesHeader.textContent = `${pages} pages`;
+
+        newBook.style.backgroundColor = `rgb(${randRed}, ${randGreen}, ${randBlue})`;
+
+
+        newBook.appendChild(titleHeader);
+        newBook.appendChild(authorHeader);
+        newBook.appendChild(pagesHeader);
+
+        let bookSection = document.querySelector('.books');
+        let newBookInfo = document.createElement('div');
+        newBookInfo.classList.add('.book-info');
+
+        let readHeader = document.createElement('h3');
+        readHeader.textContent = `${read}`;
+        newBookInfo.appendChild(newBook);
+        newBookInfo.appendChild(readHeader);
+        bookSection.appendChild(newBookInfo);
+    }
+}
+
+loadArray();
+    
+let book = document.querySelector('.book');
+let randRed = Math.floor((Math.random() * 150) + 50);
+let randGreen = Math.floor((Math.random() * 150) + 50);
+let randBlue = Math.floor((Math.random() * 200) + 1);
+book.style.backgroundColor = `rgb(${randRed}, ${randGreen}, ${randBlue})`;
